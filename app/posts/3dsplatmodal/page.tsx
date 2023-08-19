6b15524fde3bd74e60b9cf8e006ad5546bd8b827
David Huang <david.zq.huang@gmail.com>
2023-08-19 00:30:55 -0400

    use monaco

--- app/posts/3dsplatmodal/page.tsx
+++ app/posts/3dsplatmodal/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import css from "../../../styles/Post.module.css";
+import Editor from '@monaco-editor/react';
 
 export default function Post() {
     return (
@@ -10,7 +11,10 @@ export default function Post() {
                 <p className={css.postContent}>Though yesterday I think I graduated from Kindergarten in terms of Blender skills, I would very much like an easier way to create 3D assets and scenes. There is amazing paper that came out literally days ago, <a className={css.a} href="https://repo-sam.inria.fr/fungraph/3d-gaussian-splatting/">3D Gaussian Splatting for Real-Time Radiance Fields</a>, which is not only a huge improvement on the previous state-of-the-art but is also visually stunning, and seemingly good-enough-to-use and well-packaged.</p>
                 <p className={css.postContent}>My company has a bunch of GPUs but I personally do not, and 24 GB of vRAM (which is required for this model) is a strong requirement. I decided to use <a className={css.a} href="https://modal.com/">Modal Lab's</a> GPU infrastructure, which I've had previous positive experiences with.</p>
                 <p className={css.postContent}>I ran into a few issues, but the Modal Labs team was very responsive and helpful. Here is the Image setup. This will definitely be a multi-part project so the rest will have to follow later</p>
-                <pre>{`from modal import Image, Stub, method
+                <Editor
+                    height="800px"
+                    language="python"
+                    value={`from modal import Image, Stub, method
 
 stub = Stub("3dgausssplat")
 
@@ -35,7 +39,8 @@ image = (
           })
     .run_commands('TORCH_CUDA_ARCH_LIST="8.6+PTX" python -m pip install ./gaussian-splatting/submodules/diff-gaussian-rasterization', gpu="any")
     .run_commands('TORCH_CUDA_ARCH_LIST="8.6+PTX" python -m pip install ./gaussian-splatting/submodules/simple-knn', gpu="any")
-)`}</pre>
+)`}
+                ></Editor>
             </div>
         </>
     )
