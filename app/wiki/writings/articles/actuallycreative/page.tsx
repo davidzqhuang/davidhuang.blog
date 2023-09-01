58bff7ecda5ffc12b7a671cf6ddbdc79ec34f040
David Huang <david.zq.huang@gmail.com>
2023-08-31 23:03:52 -0400

    SEO?

--- app/wiki/writings/articles/actuallycreative/page.tsx
+++ app/wiki/writings/articles/actuallycreative/page.tsx
@@ -9,7 +9,8 @@ import * as U from "../../../../../components/Utils";
 const Page = () => {
     return (
         <>
-            <title>Is Generative AI Truly Creative? The Answer May Surprise You</title>
+            <title>Can Generative AI be Truly Creative? The Answer May Surprise You</title>
+            <meta name="description" content="Delving into Generative AI's potential for creativity, the article contrasts AI's outputs with iconic human masterpieces. Exploring the nuances of recognizing genius and the factors hindering AI's creative capacities - including unique training, tangible embodiment, and freedom constraints - it suggests the need for a reimagined approach to unleash AI's full innovative prowess. Discover how unlocking Generative AI's potential might bring forth the next Einstein or Dickinson in the AI realm."></meta>
             <div className='flex mb-8'>
                 <U.WikiButton />
                 <U.HomeButton />
